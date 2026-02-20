0db859ff70cbf3f3d7ecfbf98083efd22af85a42
Tricon2Elf <158119828+tricon2elf@users.noreply.github.com>
2026-02-21 12:03:49 +1300

    Refactor AreasvEnterHandler to improve constructor parameters and integrate SharedState for client registration

--- AISpace.Common/Network/Handlers/Area/AreasvEnterHandler.cs
+++ AISpace.Common/Network/Handlers/Area/AreasvEnterHandler.cs
@@ -7,16 +7,13 @@ using Microsoft.Extensions.Logging;
 
 namespace AISpace.Common.Network.Handlers;
 
-public class AreasvEnterHandler(IUserSessionRepository sessionRepo, ILogger<AreasvEnterHandler> logger) : IPacketHandler
+public class AreasvEnterHandler(ILogger<AreasvEnterHandler> _logger, IUserSessionRepository _sessionRepo, SharedState state) : IPacketHandler
 {
     public PacketType RequestType => PacketType.AreasvEnterRequest;
 
     public PacketType ResponseType => PacketType.AreasvEnterResponse;
 
     public MessageDomain Domain => MessageDomain.Area;
-
-    private readonly IUserSessionRepository _sessionRepo = sessionRepo;
-    private readonly ILogger<AreasvEnterHandler> _logger = logger;
 
     public async Task HandleAsync(ReadOnlyMemory<byte> payload, ClientConnection connection, CancellationToken ct = default)
     {
@@ -32,34 +29,12 @@ public class AreasvEnterHandler(IUserSessionRepository sessionRepo, ILogger<Area
         }
 
         connection.User = session.User;
-        uint charId = (uint)connection.User.Characters.First().Id;
+        var charId = (uint)connection.User.Characters.First().Id;
         _logger.LogInformation("Client: {ClientId} LoginRequest UserID: {UserID}, OTP: {OTP}, Name: {name}, CharID {charid}, CharName: {cname}", connection.Id, loginReq.UserID, loginReq.OTP, connection.User.Username, charId, connection.User.Characters.First().Name);
 
-        var response = new AreasvEnterResponse(0, 0);
+        state.RegisterClient("Area", connection);
+
+        var response = new AreasvEnterResponse(0, charId);
         await connection.SendAsync(ResponseType, response.ToBytes(), ct);
-
-        _ = Task.Run(
-            async () =>
-            {
-                try
-                {
-                    await Task.Delay(TimeSpan.FromSeconds(10), ct);
-                    //_logger.Info("Attempting to add Avatar");
-                    //var charaData = new CharaData(24, 24, "randomuser");
-                    //charaData.AddEquip(10100140, 0);
-                    //charaData.AddEquip(10200130, 0);
-                    //charaData.AddEquip(10100190, 0);
-                    //var avatarData = new AvatarData(1, charaData);
-                    //_ = new AvatarNotifyData(0, avatarData);
-                    //await Task.Delay(TimeSpan.FromSeconds(1), ct);
-
-                    //var moveData = new MovementData(-227.392f, -0.043f, -1418.097f, -119, MovementType.Stopped);
-                    //var moveNotify = new AvatarNotifyMove(0, (uint)connection.clientUser.Characters.First().Id, moveData);
-                    //await connection.SendAsync(PacketType.AvatarNotifyMove, moveNotify.ToBytes(), ct);
-                }
-                catch (OperationCanceledException) { }
-            },
-            ct
-        );
     }
 }
