082bda4afcb719870dbb1acffe6e685ef38b0b9d
Tricon2Elf <158119828+tricon2elf@users.noreply.github.com>
2026-02-21 12:03:09 +1300

    Add client registration and unregistration methods to SharedState

--- AISpace.Common/Game/SharedState.cs
+++ AISpace.Common/Game/SharedState.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Concurrent;
+using AISpace.Common.Network;
 
 namespace AISpace.Common.Game;
 
@@ -7,4 +8,21 @@ public class SharedState
     public ConcurrentQueue<(string id, string message)> newMessages = new();
 
     public ConcurrentQueue<(string id, MovementData moveData)> newMovement = new();
+
+    public ConcurrentDictionary<Guid, ClientConnection> MsgClients = new();
+    public ConcurrentDictionary<Guid, ClientConnection> AreaClients = new();
+
+    public void RegisterClient(string serverName, ClientConnection client)
+    {
+        if (serverName == "Msg")
+            MsgClients.TryAdd(client.Id, client);
+        else if (serverName == "Area")
+            AreaClients.TryAdd(client.Id, client);
+    }
+
+    public void UnregisterClient(string serverName, Guid clientId)
+    {
+        MsgClients.TryRemove(clientId, out _);
+        AreaClients.TryRemove(clientId, out _);
+    }
 }
