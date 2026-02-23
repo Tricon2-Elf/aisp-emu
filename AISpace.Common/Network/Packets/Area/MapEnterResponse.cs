4fa43ae81a175e5fc06b39b9d72b5a338f3cc597
subwayminded <technomaniac25@gmail.com>
2026-02-23 05:44:04 +0300

    Update MapEnterResponse.cs

--- AISpace.Common/Network/Packets/Area/MapEnterResponse.cs
+++ AISpace.Common/Network/Packets/Area/MapEnterResponse.cs
@@ -1,32 +1,23 @@
-﻿using AISpace.Common.Network;
+using AISpace.Common.Network;
 
 namespace AISpace.Common.Network.Packets.Area;
 
-public class MapEnterResponse(uint result, uint mapId, float x, float y, float z, float rot) : IPacket<MapEnterResponse>
+public class MapEnterResponse(uint result, uint mapId, uint objId, float x, float y, float z, float rot) : IPacket<MapEnterResponse>
 {
-    public uint Result = result;
-    public uint MapId = mapId;
-    public float X = x;
-    public float Y = y;
-    public float Z = z;
-    public float Rotation = rot;
-
-    public static MapEnterResponse FromBytes(ReadOnlySpan<byte> data)
-    {
-        throw new NotImplementedException();
-    }
-
     public byte[] ToBytes()
     {
         var writer = new PacketWriter();
-        writer.Write(Result);           // 0 = успех
-        writer.Write(MapId);            // ID карты
-        writer.Write(MapId);            // Serial ID (обычно совпадает с ID карты)
-        writer.Write(X);                // Координата X
-        writer.Write(Y);                // Координата Y
-        writer.Write(Z);                // Координата Z
-        writer.Write(Rotation);         // Поворот
-        writer.Write((byte)0);          // Флаг состояния (0 = стоит)
-        return writer.ToBytes();
+        writer.Write(result);
+        writer.Write(mapId); //(MapID)
+        writer.Write(mapId); //(SerialID)
+        writer.Write(objId); //(CharacterId)
+        writer.Write(x);
+        writer.Write(y);
+        writer.Write(z); 
+        writer.Write(rot);
+        writer.Write((byte)0);
+        return writer.ToBytes(); //33 bytes
     }
+
+    public static MapEnterResponse FromBytes(ReadOnlySpan<byte> data) => throw new NotImplementedException();
 }
