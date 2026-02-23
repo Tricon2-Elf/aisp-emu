94a416065a52b331a6c4018b9038e8f74d376f7d
subwayminded <technomaniac25@gmail.com>
2026-02-23 23:09:42 +0300

    Update TimeZoneGetResponse.cs

--- AISpace.Common/Network/Packets/Area/TimeZoneGetResponse.cs
+++ AISpace.Common/Network/Packets/Area/TimeZoneGetResponse.cs
@@ -1,25 +1,19 @@
-﻿using AISpace.Common.Network;
+using AISpace.Common.Network;
 
 namespace AISpace.Common.Network.Packets;
 
-public class TimeZoneGetResponse(uint result, uint timezone, uint time, uint timeZoneMax, byte flag) : IPacket<TimeZoneGetResponse>
+public class TimeZoneGetResponse(uint result, uint timezone, float time, float timeZoneMax, byte flag) : IPacket<TimeZoneGetResponse>
 {
-    public uint Result = result;
-    public uint Timezone = timezone;
-    public uint Time = time;
-    public uint TimeZoneMax = timeZoneMax;
-    public byte Flag = flag;
-
-    public static TimeZoneGetResponse FromBytes(ReadOnlySpan<byte> data) => throw new NotImplementedException();
-
     public byte[] ToBytes()
     {
         var writer = new PacketWriter();
-        writer.Write(Result);      // 4 байта
-        writer.Write(Timezone);    // 4 байта (0-4)
-        writer.Write(Time);        // 4 байта (текущий прогресс)
-        writer.Write(TimeZoneMax); // 4 байта (макс. прогресс периода)
-        writer.Write(Flag);        // 1 байт (0 или 1 для ресинка)
+        writer.Write(result);
+        writer.Write(timezone);
+        writer.Write(time);
+        writer.Write(timeZoneMax);
+        writer.Write(flag);
         return writer.ToBytes();
     }
+
+    public static TimeZoneGetResponse FromBytes(ReadOnlySpan<byte> data) => throw new NotImplementedException();
 }
