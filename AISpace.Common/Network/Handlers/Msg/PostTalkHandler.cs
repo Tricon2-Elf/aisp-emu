e32f4f1cc32e56eb40cdbf7694d79fab382b6866
subwayminded <technomaniac25@gmail.com>
2026-02-23 05:45:31 +0300

    Update PostTalkHandler.cs

--- AISpace.Common/Network/Handlers/Msg/PostTalkHandler.cs
+++ AISpace.Common/Network/Handlers/Msg/PostTalkHandler.cs
@@ -1,11 +1,10 @@
 using AISpace.Common.Network.Packets;
 using AISpace.Common.Network.Packets.Msg;
 using AISpace.Common.Game;
-using Microsoft.Extensions.Logging;
 
 namespace AISpace.Common.Network.Handlers.Msg;
 
-public class PostTalkHandler(ILogger<PostTalkHandler> logger, SharedState state) : IPacketHandler
+public class PostTalkHandler(SharedState state) : IPacketHandler
 {
     public PacketType RequestType => PacketType.PostTalkRequest;
     public PacketType ResponseType => PacketType.PostTalkResponse;
@@ -15,11 +14,9 @@ public class PostTalkHandler(ILogger<PostTalkHandler> logger, SharedState state)
     {
         var chatRequest = PostTalkRequest.FromBytes(payload.Span);
         
-        // 1. Отвечаем отправителю (ОК)
         var response = new PostTalkResponse(chatRequest.MessageID, 0);
         await connection.SendAsync(ResponseType, response.ToBytes(), ct);
 
-        // 2. Подготовка пакета для других
         var forwardPacket = new TalkForwardNotify(
             connection.CharacterId, 
             chatRequest.DistID,     
@@ -28,10 +25,8 @@ public class PostTalkHandler(ILogger<PostTalkHandler> logger, SharedState state)
         );
         byte[] broadcastData = forwardPacket.ToBytes();
 
-        // 3. Рассылаем ВСЕМ, кроме себя
         foreach (var client in state.MsgClients.Values)
         {
-            // ИСКЛЮЧАЕМ СЕБЯ (client.Id != connection.Id), чтобы не было дублей
             if (client.IsAuthenticated && client.Id != connection.Id)
             {
                 await client.SendAsync(PacketType.TalkForwardNotify, broadcastData, ct);
