79d30d2c46090de190efe9e3ac3d65655a087aec
subwayminded <technomaniac25@gmail.com>
2026-02-25 22:30:09 +0300

    Update AreaUpdateOptionHandler.cs

--- AISpace.Common/Network/Handlers/Area/AreaUpdateOptionHandler.cs
+++ AISpace.Common/Network/Handlers/Area/AreaUpdateOptionHandler.cs
@@ -1,3 +1,5 @@
+using AISpace.Common.Network.Packets;
+
 namespace AISpace.Common.Network.Handlers;
 
 public class AreaUpdateOptionHandler : IPacketHandler
@@ -8,13 +10,8 @@ public class AreaUpdateOptionHandler : IPacketHandler
 
     public async Task HandleAsync(ReadOnlyMemory<byte> payload, ClientConnection connection, CancellationToken ct = default)
     {
-        // 1. Подтверждаем получение настройки
         var writer = new PacketWriter();
-        writer.Write((uint)1); // Пробуем '1' (Успех в некоторых билдах)
+        writer.Write((uint)0); 
         await connection.SendAsync(ResponseType, writer.ToBytes(), ct);
-
-        // 2. ХИТРОСТЬ: Шлем пакет "перезагрузки" опций. 
-        // В AISp@ce это заставляет клиент обновить флаги управления (WASD)
-        await connection.SendAsync(PacketType.UpdateOptionResponse, writer.ToBytes(), ct);
     }
 }
