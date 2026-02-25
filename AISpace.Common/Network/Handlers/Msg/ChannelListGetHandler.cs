7fa9c59c8e8b19c58d467d95cf2a5502ce336fa0
subwayminded <technomaniac25@gmail.com>
2026-02-25 22:24:26 +0300

    Update ChannelListGetHandler.cs

--- AISpace.Common/Network/Handlers/Msg/ChannelListGetHandler.cs
+++ AISpace.Common/Network/Handlers/Msg/ChannelListGetHandler.cs
@@ -1,10 +1,9 @@
 using AISpace.Common.Game;
 using AISpace.Common.Network.Packets.Msg;
-using Microsoft.Extensions.Logging;
 
 namespace AISpace.Common.Network.Handlers.Msg;
 
-public class ChannelListGetHandler(ILogger<ChannelListGetHandler> logger) : IPacketHandler
+public class ChannelListGetHandler : IPacketHandler
 {
     public PacketType RequestType => PacketType.ChannelListGetRequest;
     public PacketType ResponseType => PacketType.ChannelListGetResponse;
@@ -12,14 +11,21 @@ public class ChannelListGetHandler(ILogger<ChannelListGetHandler> logger) : IPac
 
     public async Task HandleAsync(ReadOnlyMemory<byte> payload, ClientConnection connection, CancellationToken ct = default)
     {
-        // ХАРДКОД
         string myIp = "192.168.31.158"; 
-        ushort areaPort = 50054; // Порт Area сервера
-
-        logger.LogInformation($"[STEP 2] Msg -> Area (List). Sending IP: {myIp}:{areaPort}");
+        ushort areaPort = 50054;
 
         var serverInfo = new ServerInfo(myIp, areaPort);
-        var channels = new List<ChannelInfo> { new ChannelInfo(0, 0, 1, serverInfo) };
+        var channels = new List<ChannelInfo>();
+        
+        channels.Add(new ChannelInfo(1, 250, 1000, serverInfo));
+        channels.Add(new ChannelInfo(2, 0, 1000, serverInfo));
+        channels.Add(new ChannelInfo(3, 0, 1000, serverInfo));
+        channels.Add(new ChannelInfo(4, 0, 1000, serverInfo));
+        channels.Add(new ChannelInfo(5, 0, 1000, serverInfo));
+        channels.Add(new ChannelInfo(6, 0, 1000, serverInfo));
+        channels.Add(new ChannelInfo(7, 0, 1000, serverInfo));
+        channels.Add(new ChannelInfo(8, 0, 1000, serverInfo));
+
         var response = new ChannelListGetResponse(0, channels);
         await connection.SendAsync(ResponseType, response.ToBytes(), ct);
     }
