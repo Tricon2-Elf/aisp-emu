05217865d2fe842366b8780ccefd5bb0e2bb98a8
subwayminded <technomaniac25@gmail.com>
2026-02-25 22:10:49 +0300

    Update AvatarData.cs

--- AISpace.Common/Game/AvatarData.cs
+++ AISpace.Common/Game/AvatarData.cs
@@ -1,17 +1,15 @@
-﻿namespace AISpace.Common.Game;
+namespace AISpace.Common.Game;
 
 public class AvatarData(uint AvatarId, CharaData chara)
 {
     public byte[] ToBytes()
     {
         var writer = new Network.PacketWriter();
-        writer.Write(AvatarId); // 4 байта
-        writer.Write(chara.ToBytes()); // 383 байта
-        writer.Write((ushort)8); // Флаг/разделитель (2 байта)
-        
-        // Паддинг: 928 - 4 - 383 - 2 = 539 байт
+        writer.Write(AvatarId);
+        writer.Write(chara.ToBytes());
+        writer.Write((ushort)8);
         writer.Write(new byte[539]); 
         
-        return writer.ToBytes(); // Итого 928 байт
+        return writer.ToBytes();
     }
 }
