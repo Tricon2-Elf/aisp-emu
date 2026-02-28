0f6fc0143d95b7ff104885835ac769f55bf24131
Tricon2Elf <158119828+tricon2elf@users.noreply.github.com>
2026-02-28 17:02:37 +1300

    Fix wrong response result

--- AISpace.Common/Network/Handlers/Area/AreaMapLinkGetDataHandler.cs
+++ AISpace.Common/Network/Handlers/Area/AreaMapLinkGetDataHandler.cs
@@ -15,9 +15,9 @@ public class AreaMapLinkGetDataHandler(ILogger<AreaMapLinkGetDataHandler> logger
     {
         var request = MapLinkGetDataRequest.FromBytes(payload.Span);
         logger.LogCritical("MapLinkGetDataRequest received from user {UserId} on map {MapId} with channel {ChannelId}", connection.User.Id, request.MapId, request.ChannelId);
-        var response = new MapLinkGetDataResponse(1);
+        var response = new MapLinkGetDataResponse(0);
         await connection.SendAsync(ResponseType, response.ToBytes(), ct);
-        var maplinkAtPlayer = new MapLinkData(connection.X, connection.Y, connection.Z - 1000f, 0, 1000f, 10f);
+        var maplinkAtPlayer = new MapLinkData(connection.X, connection.Y, connection.Z, 0, 1000f, 1000f);
         await connection.SendAsync(PacketType.MapLinkNotifyData, new MapLinkNotifyData(0, maplinkAtPlayer).ToBytes(), ct);
         // Tell client where this maplink goes (same order as maplinks)
         await connection.SendAsync(PacketType.NotifySelectMap, new NotifySelectMapData(10990110u).ToBytes(), ct);
