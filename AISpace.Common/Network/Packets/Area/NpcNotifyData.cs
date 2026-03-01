dac8e10fb19e8815b2b815a139f1e48f841411cb
Tricon2Elf <158119828+tricon2elf@users.noreply.github.com>
2026-03-01 22:23:52 +1300

    NPC Testdata

---
+++ AISpace.Common/Network/Packets/Area/NpcNotifyData.cs
@@ -0,0 +1,33 @@
+using AISpace.Common.Game;
+using AISpace.Common.Network;
+
+public class NpcNotifyData(uint result, uint npcObjectId, CharaData charaData) : IPacket<NpcNotifyData>
+{
+    private const int EntityPrefixSize = 366;
+    private const int Sub798D80Size = 175;
+    private const int Sub798B10Size = 25;
+
+    public static NpcNotifyData FromBytes(ReadOnlySpan<byte> data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public byte[] ToBytes()
+    {
+        var writer = new PacketWriter();
+        writer.Write(result);
+        writer.Write(npcObjectId);
+
+        var entity = charaData.ToBytes();
+        int prefixLen = Math.Min(entity.Length, EntityPrefixSize);
+        writer.Write(entity.AsSpan(0, prefixLen));
+        if (prefixLen < EntityPrefixSize)
+            writer.Write(new byte[EntityPrefixSize - prefixLen]);
+
+        writer.Write(new byte[Sub798D80Size]);
+        writer.Write(new byte[Sub798B10Size]);
+
+        writer.Write((byte)0); // trailing byte read by ReadNpcData
+        return writer.ToBytes();
+    }
+}
