5b8868644313602bd80ae2b525ab9849ae8c1c6d
Tricon2Elf <158119828+tricon2elf@users.noreply.github.com>
2026-03-01 19:28:08 +1300

    Add ChannelId to AreaMapEnterRequest

--- AISpace.Common/Network/Packets/Area/AreaMapEnterRequest.cs
+++ AISpace.Common/Network/Packets/Area/AreaMapEnterRequest.cs
@@ -3,11 +3,12 @@ namespace AISpace.Common.Network.Packets.Area;
 public class AreaMapEnterRequest : IPacket<AreaMapEnterRequest>
 {
     public uint MapID { get; set; }
+    public uint ChannelId { get; set; }
 
     public static AreaMapEnterRequest FromBytes(ReadOnlySpan<byte> data)
     {
         var reader = new PacketReader(data);
-        return new AreaMapEnterRequest { MapID = reader.ReadUInt() };
+        return new AreaMapEnterRequest { MapID = reader.ReadUInt(), ChannelId = reader.ReadUInt() };
     }
 
     public byte[] ToBytes() => throw new NotImplementedException();
