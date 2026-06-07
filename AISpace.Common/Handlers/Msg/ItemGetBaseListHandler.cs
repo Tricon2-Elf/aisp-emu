12c18a8ca094226512cd6764d587fc98607ed8ea
Tricon2-Elf <1232321+Tricon2-Elf@users.noreply.github.com>
2026-06-07 22:25:38 +1200

    Added caching for ItemGetBaseListHandler due to its roughly 1.8MB size

--- AISpace.Common/Handlers/Msg/ItemGetBaseListHandler.cs
+++ AISpace.Common/Handlers/Msg/ItemGetBaseListHandler.cs
@@ -1,11 +1,12 @@
 using AISpace.Common.DAL.Repositories;
 using AISpace.Common.Game;
+using AISpace.Common.Services;
 using AISpace.Network;
 using AISpace.Network.Packets.Msg;
 
 namespace AISpace.Common.Handlers.Msg;
 
-public class ItemGetBaseListHandler(IItemRepository itemRepo) : IPacketHandler
+public class ItemGetBaseListHandler(IItemBaseListCache cache) : IPacketHandler
 {
     public PacketType RequestType => PacketType.ItemGetBaseListRequest;
     public PacketType ResponseType => PacketType.ItemGetBaseListResponse;
@@ -13,9 +14,9 @@ public class ItemGetBaseListHandler(IItemRepository itemRepo) : IPacketHandler
 
     public async Task HandleAsync(ReadOnlyMemory<byte> payload, IPlayerSession session, CancellationToken ct = default)
     {
-        var rows = await itemRepo.GetAllAsync(ct);
-        var items = rows.Select(ItemEntityMapper.ToItemBaseListData).ToList();
-        var response = new ItemGetBaseListResponse(0, items);
-        await session.SendAsync(ResponseType, response.ToBytes(), ct);
+        if (cache.ResponsePayload.IsEmpty)
+            await cache.WarmAsync(ct);
+
+        await session.SendAsync(ResponseType, cache.ResponsePayload.ToArray(), ct);
     }
 }
