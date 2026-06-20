23a427455021b43a33f2b26686ebae271e0de0e3
Tricon2-Elf <1232321+Tricon2-Elf@users.noreply.github.com>
2026-06-21 10:54:19 +1200

    Cleanup CircleData

--- AISpace.Network/Data/CircleData.cs
+++ AISpace.Network/Data/CircleData.cs
@@ -2,26 +2,24 @@ namespace AISpace.Network.Data;
 
 public class CircleData(uint id, string name, uint leaderId)
 {
-    public uint Id = id;
-    public string Name = name;
-    public uint LeaderId = leaderId;
+    public uint Id { get; set; } = id;
+    public uint Status { get; set; } = 1;        // circle status flag
+    public string Name { get; set; } = name;
+    public uint LeaderId { get; set; } = leaderId;
+    public string Date { get; set; } = "";
+    public byte[] Unk_20 { get; set; } = new byte[20]; // unknown
+    public string Message { get; set; } = "";
 
     public byte[] ToBytes()
     {
         var writer = new PacketWriter();
-
         writer.Write(Id);
-        writer.Write((uint)1); // Status
-
+        writer.Write(Status);
         writer.WriteFixedString(Name, 46, "Shift_JIS");
-
         writer.Write(LeaderId);
-
-        writer.Write(new byte[37]);
-
-        writer.Write(new byte[20]);
-
-        writer.Write(new byte[751]);
+        writer.WriteFixedString(Date, 37, "Shift_JIS");
+        writer.Write(Unk_20);
+        writer.WriteFixedString(Message, 751, "Shift_JIS");
         return writer.ToBytes();
     }
 }
