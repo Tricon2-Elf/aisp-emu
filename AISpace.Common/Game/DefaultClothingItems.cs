8c8accffc1ecf511397e778f1dc8a3665492d5a2
Tricon2-Elf <1232321+Tricon2-Elf@users.noreply.github.com>
2026-06-21 10:00:00 +1200

    Remove unneeded item from Male default clothing

--- AISpace.Common/Game/DefaultClothingItems.cs
+++ AISpace.Common/Game/DefaultClothingItems.cs
@@ -2,7 +2,7 @@ namespace AISpace.Common.Game;
 
 internal static class DefaultClothingItems
 {
-    public static readonly IReadOnlyList<int> Male = [10100220, 10200100, 10400030, 10500070, 10700020];
+    public static readonly IReadOnlyList<int> Male = [10100220, 10200100, 10400030, 10500070];
 
     public static readonly IReadOnlyList<int> Female = [10100060, 10200090, 10400000, 10500010];
 
