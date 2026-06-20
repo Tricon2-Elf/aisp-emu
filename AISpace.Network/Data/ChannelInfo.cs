a5d2af4f41616e3161be829ade5512b2e75ab48e
Tricon2-Elf <1232321+Tricon2-Elf@users.noreply.github.com>
2026-06-21 10:36:11 +1200

    Clean up ChannelInfo

--- AISpace.Network/Data/ChannelInfo.cs
+++ AISpace.Network/Data/ChannelInfo.cs
@@ -1,21 +1,15 @@
 namespace AISpace.Network.Data;
 
-public class ChannelInfo(uint channelID, uint currentUserCount, uint maxUserCount, ServerInfo serverInfo)
+public class ChannelInfo(uint ChannelId, uint CurrentUserCount, uint MaxUserCount, ServerInfo ServerInfo)
 {
-    public const int PacketSize = 4 + 4 + 4 + 67;
-
-    public uint channelID = channelID;
-    public uint currentUserCount = currentUserCount;
-    public uint maxUserCount = maxUserCount;
-    public ServerInfo serverInfo = serverInfo;
 
     public byte[] ToBytes()
     {
         var writer = new PacketWriter();
-        writer.Write(channelID);
-        writer.Write((float)currentUserCount);
-        writer.Write(maxUserCount);
-        writer.Write(serverInfo.ToBytes());
+        writer.Write(ChannelId);
+        writer.Write((float)CurrentUserCount);
+        writer.Write(MaxUserCount);
+        writer.Write(ServerInfo.ToBytes());
         return writer.ToBytes();
     }
 }
