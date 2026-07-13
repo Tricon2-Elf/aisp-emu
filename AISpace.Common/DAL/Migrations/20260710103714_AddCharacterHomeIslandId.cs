c9c623ad74bff9536eed2a7af090107d2897d19b
Tricon2-Elf <1232321+Tricon2-Elf@users.noreply.github.com>
2026-07-13 22:19:27 +1200

    Add HomeIslandId property to Character entity

---
+++ AISpace.Common/DAL/Migrations/20260710103714_AddCharacterHomeIslandId.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AISpace.Common.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCharacterHomeIslandId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<uint>(name: "HomeIslandId", table: "Characters", type: "INTEGER", nullable: false, defaultValue: 0u);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "HomeIslandId", table: "Characters");
+        }
+    }
+}
