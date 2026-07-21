4d53267b7b218c901365cf98ddb780a3de9e677d
Tricon2-Elf <1232321+Tricon2-Elf@users.noreply.github.com>
2026-07-21 21:48:27 +1200

    Add some TPS packets

---
+++ AISpace.Network/Packets/Area/BattleTargetLockRequest.cs
@@ -0,0 +1,12 @@
+namespace AISpace.Network.Packets.Area;
+
+public sealed class BattleTargetLockRequest(uint targetObjectId) : IIncomingPacket<BattleTargetLockRequest>
+{
+    public uint TargetObjectId { get; } = targetObjectId;
+
+    public static BattleTargetLockRequest FromBytes(ReadOnlySpan<byte> data)
+    {
+        var reader = new PacketReader(data);
+        return new BattleTargetLockRequest(reader.ReadUInt());
+    }
+}
