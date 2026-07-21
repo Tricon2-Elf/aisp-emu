ffd7b5c7d2bbd2269e8b4f1c17d586d602c2d9d1
Tricon2-Elf <1232321+Tricon2-Elf@users.noreply.github.com>
2026-07-21 21:48:27 +1200

    Add some TPS packets

---
+++ AISpace.Network/Packets/Area/BattleResultResponse.cs
@@ -0,0 +1,13 @@
+namespace AISpace.Network.Packets.Area;
+
+public sealed class BattleResultResponse(uint result = 0) : IOutgoingPacket
+{
+    public uint Result { get; } = result;
+
+    public byte[] ToBytes()
+    {
+        var writer = new PacketWriter();
+        writer.Write(Result);
+        return writer.ToBytes();
+    }
+}
