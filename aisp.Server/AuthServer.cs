cf10e84cc4d07422422122bcb20b4a00b1384333
Tricon2-Elf <1232321+Tricon2-Elf@users.noreply.github.com>
2026-08-27 22:15:16 +1200

    Remove creation of testuser as portal is up and running

--- aisp.Server/AuthServer.cs
+++ aisp.Server/AuthServer.cs
@@ -1,9 +1,5 @@
 using aisp.Common;
-using aisp.Common.DAL;
-using aisp.Common.DAL.Repositories;
 using aisp.Common.Game;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace aisp.Server;
 
@@ -11,13 +7,4 @@ public class AuthServer(ILogger<AuthServer> logger, GameServerContext ctx, int p
     : GameServerBase<AuthServer>(logger, ctx, port)
 {
     protected override ServerType ActiveServerType => ServerType.Auth;
-
-    protected override async Task InitializeAsync(CancellationToken ct)
-    {
-        using var scope = ScopeFactory.CreateScope();
-        var db = scope.ServiceProvider.GetRequiredService<MainContext>();
-        var userRepo = scope.ServiceProvider.GetRequiredService<IUserRepository>();
-        if (!await db.Users.AnyAsync(ct))
-            await userRepo.AddAsync("testuser", "password");
-    }
 }
